0644615bc0b84fc0cb3467ba198bdb24de6f4237
Sorin Chis <kish.sorin@yahoo.com>
2019-03-04 13:48:16 +0200

    added functionality to increase , decrease function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { INCREMENT, DECREMENT } from '../actions';
 
 const initialState = {
-  count: 0
-}
+	count: 0
+};
 
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
@@ -10,12 +10,21 @@ const initialState = {
 // by our action creator. What does the reducer
 // need to do with the count in each case?
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-    // Fill in the body of this case
-    case DECREMENT:
-    // Fill in the body of this case
-    default:
-      return state;
-  }
+	console.log('reducer: ', action);
+	switch (action.type) {
+		case INCREMENT:
+			// Fill in the body of this case
+			return {
+				...state,
+				count: state.count + action.payload
+			};
+		case DECREMENT:
+			// Fill in the body of this case
+			return {
+				...state,
+				count: state.count - action.payload
+			};
+		default:
+			return state;
+	}
 };
