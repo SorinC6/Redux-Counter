d6db4e7eed3409f65fb4ad28d94b7eec52dde943
Sorin Chis <kish.sorin@yahoo.com>
2019-03-04 13:59:35 +0200

    added stretch task

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
+import { bindActionCreators } from 'redux';
 
 class Counter extends Component {
 	incrementIfOdd = () => {
 		// Stretch Problem: Implement an increment function that
 		// only increments if the counter value is odd
+		if (this.props.count % 2 !== 0) {
+			this.props.increment(1);
+		} else {
+			return;
+		}
 	};
 
 	incrementAsync = () => {
 		// Stretch Problem: Implement an increment function that
 		// increments after waiting for one second
+		setTimeout(() => {
+			this.props.increment(1);
+		}, 1000);
 	};
 
 	render() {
@@ -24,12 +33,8 @@ class Counter extends Component {
 				<button onClick={() => this.props.decrement(1)}>-</button>
 				{/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-				{/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+				<button onClick={this.incrementIfOdd}>Increment if odd</button>
+				<button onClick={this.incrementAsync}>Increment async</button>
 			</p>
 		);
 	}
@@ -47,9 +52,19 @@ const mapStateToProps = (state) => {
 	};
 };
 
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators(
+		{
+			increment,
+			decrement
+		},
+		dispatch
+	);
+}
+
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
-export default connect(mapStateToProps, { increment, decrement })(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
